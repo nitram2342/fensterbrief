4b21df9aeab6e0fd2dce7dd56ef856ee9eeee1f7
Martin Schobert <martin@schobert.cc>
2017-01-11 15:07:31 +0100

    dependency added

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['configparser', 'python-slugify', 'zeep', 'pandoc', 'pyyaml'],
+    install_requires=['configparser', 'python-slugify', 'zeep', 'pandoc', 'pyyaml', 'googlemaps'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
