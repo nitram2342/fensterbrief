acec16228f8db7576c49c254f4b257235017685f
Martin Schobert <martin@schobert.cc>
2017-02-03 14:34:38 +0100

    version number increased

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.15',
+    version='0.0.16',
 
     description='Fensterbrief is a python script for working with letters based on LaTeX and Markdown.',
     long_description=long_description,
